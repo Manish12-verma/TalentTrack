1e3274125845facf591526c18c2457e5e9c3bb8f
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-01-28 22:41:19 +0530

    created admin dashboard

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,46 @@
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className="h-[55%] w-full overflow-x-auto py-5 mt-10 flex items-center justify-start gap-5 flex-nowrap">
+          <div className="flex-shrink-0 h-full w-[350px] p-5 bg-green-400 rounded-xl ">
+                 <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 text-small px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm font-semibold">20 Feb 2024</h4>
+                 </div>
+                 <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+                 <p className="text-sm mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore soluta itaque consectetur laudantium modi aspernatur placeat laboriosam maxime. Illum officia repellat perspiciatis pariatur tenetur voluptates sequi dolor eligendi officiis rerum.</p>
+          </div>
+          <div className="flex-shrink-0 h-full w-[350px] p-5 bg-yellow-400 rounded-xl ">
+                 <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 text-small px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm font-semibold">20 Feb 2024</h4>
+                 </div>
+                 <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+                 <p className="text-sm mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore soluta itaque consectetur laudantium modi aspernatur placeat laboriosam maxime. Illum officia repellat perspiciatis pariatur tenetur voluptates sequi dolor eligendi officiis rerum.</p>
+          </div>
+          
+          <div className="flex-shrink-0 h-full w-[350px] p-5 bg-blue-400 rounded-xl ">
+                 <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 text-small px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm font-semibold">20 Feb 2024</h4>
+                 </div>
+                 <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+                 <p className="text-sm mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore soluta itaque consectetur laudantium modi aspernatur placeat laboriosam maxime. Illum officia repellat perspiciatis pariatur tenetur voluptates sequi dolor eligendi officiis rerum.</p>
+          </div>
+          
+          <div className="flex-shrink-0 h-full w-[350px] p-5 bg-red-400 rounded-xl ">
+                 <div className="flex justify-between items-center">
+                    <h3 className="bg-red-600 text-small px-3 py-1 rounded">High</h3>
+                    <h4 className="text-sm font-semibold">20 Feb 2024</h4>
+                 </div>
+                 <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+                 <p className="text-sm mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore soluta itaque consectetur laudantium modi aspernatur placeat laboriosam maxime. Illum officia repellat perspiciatis pariatur tenetur voluptates sequi dolor eligendi officiis rerum.</p>
+          </div>
+          
+          
+          
+    </div>
+  )
+}
+
+export default TaskList
