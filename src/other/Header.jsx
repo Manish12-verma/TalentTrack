b2f8445ff41cab22a3bf0345203335a0a60e3867
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-01-30 15:17:53 +0530

    added logout functionality

--- src/other/Header.jsx
+++ src/other/Header.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react"
 
 const Header = () => {
+
+  // const [username,setUsername] = useState('')
+
+  // if(!data){
+  //   setUsername('Admin')
+  // }else{
+  //   setUsername(data.firstName)
+  // }
+
+  const logoutHandler=()=>{
+   
+     localStorage.setItem('loggedInUser','')
+     window.location.reload()
+  }
+
   return (
     <div className="flex items-end justify-between">
          <h1 className="text-2xl font-medium text-white">Hello, <br /> <span className="text-3xl font-semibold">Manish👋</span> </h1>
-         <button className="bg-red-600 text-white px-3 py-2 rounded text-lg font-medium">Log Out</button>
+         <button className="bg-red-600 text-white px-3 py-2 rounded text-lg font-medium" onClick={logoutHandler}>Log Out</button>
     </div>
   )
 }
--- src/other/TaskListNumbers.jsx
+++ src/other/TaskListNumbers.jsx
@@ -1,21 +1,22 @@
+/* eslint-disable react/prop-types */
 
-const TaskListNumbers = () => {
+const TaskListNumbers = ({data}) => {
   return (
     <div className="flex screen justify-between gap-5 mt-10 ">
              <div className="w-[45%] bg-red-400 rounded-xl py-6 px-9">
-                   <h2 className="text-3xl font-semibold">0</h2>
+                   <h2 className="text-3xl font-semibold">{data.taskNumbers.newTask}</h2>
                    <h3 className="text-xl font-medium">New Task</h3>
              </div>
              <div className="w-[45%] bg-blue-400 rounded-xl py-6 px-9">
-                   <h2 className="text-3xl font-semibold">2</h2>
+                   <h2 className="text-3xl font-semibold">{data.taskNumbers.completed}</h2>
                    <h3 className="text-xl font-medium">Completed Task</h3>
              </div>
              <div className="w-[45%] bg-green-400 rounded-xl py-6 px-9">
-                   <h2 className="text-3xl font-bold text-black">3</h2>
+                   <h2 className="text-3xl font-bold text-black">{data.taskNumbers.active}</h2>
                    <h3 className="text-xl font-medium ">Accepted Task</h3>
              </div>
              <div className="w-[45%] bg-yellow-400 rounded-xl py-6 px-9">
-                   <h2 className="text-3xl font-semibold">0</h2>
+                   <h2 className="text-3xl font-semibold">{data.taskNumbers.failed}</h2>
                    <h3 className="text-xl font-medium">Failed Task</h3>
              </div>
     </div>
